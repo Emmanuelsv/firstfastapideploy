a14763e9e759bb21b4887c6d7344677c262f31d7
Emmanuelsv <125302731+Emmanuelsv@users.noreply.github.com>
2025-04-18 18:17:45 -0500

    directorios de la api del curso de Mouredev

---
+++ routers2/products.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix= "/products",
+                   tags=["products"], 
+                   responses={404:{"message": "no encontrado"}})
+
+product_list = ["Product 1", "Product 2", 
+                "Product 3", "Product 4", "Product 5"]
+
+@router.get("/")
+async def products():
+    return product_list
+
+
+@router.get("/{id}")
+async def products(id: int):
+    return product_list[id]
